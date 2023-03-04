a3cc537f1afa67d6b6130e155a9ff925ac01a132
Cong Minh <minh.tc03208@sinhvien.hoasen.edu.vn>
2023-03-04 10:21:37 +0700

    .

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'lobby', loadChildren: () => import('./pages/lobby/lobby.module').then(m => m.LobbyModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
