df849c7aab2bc07f072cf79707ddac1fd5b13c73
ThienNhan <nhan.dt12793@sinhvien.hoasen.edu.vn>
2023-03-04 17:04:59 +0700

    .

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
-{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
+{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+{ path: 'lobby', loadChildren: () => import('./pages/lobby/lobby.module').then(m => m.LobbyModule) },
+{ path: 'createquestion', loadChildren: () => import('./pages/createquestion/createquestion.module').then(m => m.CreatequestionModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ client/src/app/pages/createquestion/createquestion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createquestion',
+  templateUrl: './createquestion.component.html',
+  styleUrls: ['./createquestion.component.scss']
+})
+export class CreatequestionComponent {
+
+}
