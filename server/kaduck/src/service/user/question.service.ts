6a76aed2b777ddc80d17f83513fa23c1bd1e0aec
Cong Minh <minh.tc03208@sinhvien.hoasen.edu.vn>
2023-03-09 13:16:48 +0700

    .

--- server/kaduck/src/service/user/question.service.ts
+++ server/kaduck/src/service/user/question.service.ts
@@ -1,6 +1,6 @@
+import { InjectModel } from '@nestjs/mongoose';
 /* eslint-disable prefer-const */
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Question, UserDocument } from 'src/schemas/question.schema';
 
