cad7356072c96df5ecfdcc69fba5c7266cabe39d
Thanh Thien <thien.mlt8601@sinhvien.hoasen.edu.vn>
2023-03-12 00:02:28 +0700

    update server

--- server/kaduck/src/question_kit/question_kit.service.ts
+++ server/kaduck/src/question_kit/question_kit.service.ts
@@ -12,14 +12,14 @@ export class QuestionKitService {
        {}
        async getAll(): Promise<Question_Kit[]> {
         try{
-            return this.questionKitModel.find().populate('questions').exec();
+            return this.questionKitModel.find().populate('question_kit').exec();
         }catch(error){
             console.log(error);
         }
         }
         async getById(id:string): Promise<Question_Kit> {
             try{
-                return await this.questionKitModel.findOne({id: id}).populate('questions',this.questionModel).exec();
+                return await this.questionKitModel.findOne({id: id}).populate('question_kit',this.questionModel).exec();
             }catch(error){
                 console.log(error);
             }
@@ -27,14 +27,63 @@ export class QuestionKitService {
         async create(questionKit: Question_Kit){
             try{
                 let ques =  questionKit.question_kit;
-                questionKit.question_kit=[];
-                questionKit.id=Math.floor(Math.random() * 1000 ).toString();
+                ques=[];
+                questionKit.id=Date.now().toString();
                 let nqk = await this.questionKitModel.create(questionKit);
                 
+                await ques.forEach(async (q:any)=>{
+                    q.id = Date.now().toString();
+                    let newQues= await this.questionModel.create(q);
+                    
+                    await this.questionKitModel.findOneAndUpdate({id: nqk.id},{$push: {ques: newQues._id}});
+                })
+                return nqk;
             }catch(error){
                 console.log(error);
             }
 
             
+        }
+        async update(id:string ,questionKit: Question_Kit) {
+            try{
+                await this.questionKitModel.findOneAndUpdate({id: id},questionKit)
+                return questionKit;
+            }catch(error){
+                console.log(error);
+            }
+        }
+        async pushQuestionToKit(id:string ,questionId: string) {
+            try{
+                let newQuestion= await this.questionModel.findOne(
+                    {id: questionId},
+                    ).exec();
+    
+                await this.questionKitModel.findOneAndUpdate(
+                    {id: id},
+                    { $push: {
+                        question_kit: newQuestion._id
+                    }}
+                )
+                return true;
+            }catch(error){
+                console.log(error);
+            }
+        }
+        async popQuestionFromKit(id:string ,questionId: string) {
+            try{
+                let newQuestion= await this.questionModel.findOne(
+                    {id: questionId},
+                    ).exec();
+    
+                await this.questionKitModel.findOneAndUpdate(
+                    {id: id},
+                    { $pull: {
+                        question_kit: newQuestion._id
+                    }}
+                )
+                return true;
+            }catch(error){
+                console.log(error);
+            }
         }
 }
